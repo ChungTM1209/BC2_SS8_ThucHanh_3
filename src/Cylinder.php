a2fb93bc28f5d4352f96dd0ec830c83ee1031a18
ChungTM1209 <minhchung.ctk17@gmail.com>
2019-01-30 11:38:24 +0700

    THuc Hanh 3

--- src/Cylinder.php
+++ src/Cylinder.php
@@ -10,9 +10,21 @@ class Cylinder
 {
     public function getVolume($radius, $height)
     {
-        $baseArea = pi() * $radius * $radius;
-        $perimeter = 2 * pi() * $radius;
+        $baseArea = $this->getBaseArea($radius);
+        $perimeter = $this->getPerimeter($radius);
         $volume = $perimeter * $height + 2 * $baseArea;
         return $volume;
+    }
+
+    public function getPerimeter($radius)
+    {
+        $baseArea = pi() * $radius * $radius;
+        return $baseArea;
+    }
+
+    public function getBaseArea($radius)
+    {
+        $perimeter = 2 * pi() * $radius;
+        return $perimeter;
     }
 }
